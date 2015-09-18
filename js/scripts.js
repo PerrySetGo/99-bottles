566048aec4aa9819dd8ea8d65ea4a2a60ff168d5
Perry Eising <perry.r.eising@gmail.com>
2015-09-18 13:18:34 -0700

    change logic and fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -15,16 +15,21 @@ while ( newBottleCount > 0)
   {
     //htmlString = newBottleCount + "bottles of beer on the wall";
     htmlString = newBottleCount + "bottles of beer on the wall," + newBottleCount + "bottles of beer. Take it down and pass it around, " + newBottleCount + "bottles of beer on the wall.";
-    console.log("newBottleCount");
+    console.log("newBottleCount1"+newBottleCount); 
     newBottleCount--; 
+    console.log("newBottleCount2"+newBottleCount);
   return newBottleCount; 
   }
 }; 
 
 $(document).ready(function() {
     $("form#bottleCount").submit(function(event) {
+      $(".lyrics").empty();
     var mainInput = $("input#mainInput").val(); //add functionality to handle null values
     var songText = bottleCount(mainInput);
+
+
+
 
     $(".lyrics").append(songText);
 
