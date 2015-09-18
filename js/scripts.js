34052bf2e67887bdff3c7f5ce51e166ae16052d4
Perry Eising <perry.r.eising@gmail.com>
2015-09-18 13:44:31 -0700

    change bottlecount function to call functions for lyric verses

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,18 @@
 
-
-
-
-
-
-
-var bottleCount = function(mainInput){
-
-debugger; 
-var newBottleCount = mainInput; 
-var htmlString = "";
-
-while ( newBottleCount > 0)
-  {
-    //htmlString = newBottleCount + "bottles of beer on the wall";
-    htmlString = newBottleCount + "bottles of beer on the wall," + newBottleCount + "bottles of beer. Take it down and pass it around, " + newBottleCount + "bottles of beer on the wall.";
-    console.log("newBottleCount1"+newBottleCount); 
-    newBottleCount--; 
-    console.log("newBottleCount2"+newBottleCount);
-  return newBottleCount; 
+var bottleCount = function(number) {
+  if (number == 0) {
+    return finalLyrics();
+  } else {
+    return repeatedLyrics(number) + bottlesOfBeer(number-1);
   }
-}; 
+}
+
 
 $(document).ready(function() {
     $("form#bottleCount").submit(function(event) {
-      $(".lyrics").empty();
+    $(".lyrics").empty();
     var mainInput = $("input#mainInput").val(); //add functionality to handle null values
     var songText = bottleCount(mainInput);
-
-
-
 
     $(".lyrics").append(songText);
 
